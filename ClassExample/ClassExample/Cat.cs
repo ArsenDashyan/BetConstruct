46aa0d1dfe2599dcf9f1b5b87a664930f48ef685
Arsen Dashyan <arsen_dashyan@outlook.com>
2021-06-03 21:18:24 +0400

    Added two classes Product and Cut and creat constructors

---
+++ ClassExample/ClassExample/Cat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample
+{
+    class Cat
+    {
+        public string Name;
+        public double Age;
+        public string Color;
+        public bool IsMale;
+
+        public Cat(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public Cat(string Name, double Age) :this(Name)
+        {
+            this.Age = Age;
+        }
+
+        public Cat(string Name, double Age, string Color) : this(Name, Age)
+        {
+            this.Color = Color;
+        }
+
+        public Cat(string Name, double Age, string Color, bool IsMale) : this(Name, Age, Color)
+        {
+            this.IsMale = IsMale;
+        }
+    }
+}
---
+++ ClassExample/ClassExample/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassExample
+{
+    class Product
+    {
+        public string ProductName;
+        public double ProductPrice;
+        public double ProductQuantity;
+        public bool Active;
+
+
+        public Product(string productName)
+        {
+            ProductName = productName;
+        }
+
+        public Product(string productName, double productPrice) : this(productName)
+        {
+            ProductPrice = productPrice;
+        }
+
+        public Product(string productName, double productPrice, double productQuantity) : this(productName, productPrice)
+        {
+            ProductQuantity = productQuantity;
+        }
+
+        public Product(string productName, double productPrice, double productQuantity, bool active) : this(productName, productPrice, productQuantity)
+        {
+            Active = active;
+        }
+    }
+}
